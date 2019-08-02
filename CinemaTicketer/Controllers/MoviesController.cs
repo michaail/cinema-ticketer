501973a36d73d2498e1ff44f2f27835fc4078c3f
Michal Klos <galeo09@gmail.com>
2019-08-02 21:25:59 +0200

    Updated views

--- CinemaTicketer/Controllers/MoviesController.cs
+++ CinemaTicketer/Controllers/MoviesController.cs
@@ -54,7 +54,7 @@ namespace CinemaTicketer.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Duration,Description,Rating,Director,Cast,PosterUrl")] Movie movie)
+        public async Task<IActionResult> Create([Bind("Id,Name,Duration,Description,Rating,Director,Cast,PosterUrl,PremiereDate")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace CinemaTicketer.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Duration,Description,Rating,Director,Cast,PosterUrl")] Movie movie)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Duration,Description,Rating,Director,Cast,PosterUrl,PremiereDate")] Movie movie)
         {
             if (id != movie.Id)
             {
